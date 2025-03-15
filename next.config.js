c0dd697f7aa94669240015a82232d51fa7d3478f
Michael Zampiglia <your-email@example.com>
2025-03-14 17:44:31 -0700

    Update next.config.js to allow embedding in GoHighLevel iframe

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' *.gohighlevel.com app.gohighlevel.com *.leadconnectorhq.com;"
+          }
+        ]
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig 
